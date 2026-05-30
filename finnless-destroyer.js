a7046c5f6d11f475891a4bfb4b5d2667ea4517bf
xyntercept <ramenoodlez10@gmail.com>
2026-05-30 17:30:50 +0400

    Update finnless-destroyer.js

--- finnless-destroyer.js
+++ finnless-destroyer.js
@@ -27,9 +27,19 @@ v1.3
 v1.3.1
 2026-05-28
 - Tweaked how FtHoF results are collected, improving efficiency nearly twofold
+
+v1.4
+2026-05-30
+- Fixed a bug where 7x offset could not be tracked
+- Improved efficiency again by changing how randoim values are collected
+- Reorganized the mod's options and added a heading and description
+- Added a button to export save with correct number of spells
 */
 
 // Will this actually destroy finnless? Remains to be seen
+
+let firstComboLoc = 0;
+
 // I also love stealing code from sisisem
 function setLookahead() {
   Game.Prompt('<id ImportSave><h3>'+"Input value"+'</h3><div class="block">'+loc("Input to modify the variable.")+'<div id="importError" class="warning" style="font-weight:bold;font-size:11px;"></div></div><div class="block"><textarea id="textareaPrompt" style="width:100%;height:128px;">'+'</textarea></div>',[[loc("Confirm"),';Game.ClosePrompt(); Game.prefs.lookahead=Number((l(\'textareaPrompt\').value));'],loc("Cancel")]);
@@ -46,34 +56,14 @@ function setFthofNeeded() {
   l('textareaPrompt').focus();
 }
 
-function getFthofResult(spellCount,change = 0) {
-  Math.seedrandom(Game.seed+'/'+spellCount);
-  Math.random();
-  Math.random();
-  Math.random();
-  if (change == 1) Math.random();
-  choices = [ ];
-  choices.push("Frenzy","Lucky","Click Frenzy");
-  if (Math.random() < 0.1) choices.push('Cookie Storm', 'Cookie Storm', 'Blab');
-  if (Math.random() < 0.25) choices.push('Building Special');
-  if (Math.random() < 0.15) choices = ['Cookie Storm Drop'];
-  if (Math.random() < 0.0001) choices.push('Free Sugar Lump');
-  return choose(choices);
-}
-
 function isResolvable(index,spellsList,spells) {
   let points = 0;
 
   if (spellsList[index][0] > 0.125 && spellsList[index][0] < 0.25) {
-    for (let i = 1; i < 7; i++) { 
-      if (index+i >= spellsList.length) return 0;
-
-      if (spellsList[index+i][1] == 0) {
-        spellsList[index+i][1] = getFthofResult(spells+index+i);
-        spellsList[index+i][2] = getFthofResult(spells+index+i,1);
-      }
-      
-      if ((spellsList[index+i][1] === "Building Special" || spellsList[index+i][2] === "Building Special") && spellsList[index+i][0] < 0.5) return 1;
+    for (let i = 1; i <= 7; i++) { 
+      if (index+i >= spellsList.length) return 0;    
+      if (spellsList[index+i][1] == 1 || spellsList[index+i][2] == 1) return 1;
+      else if (i == 7) break;
       else if (spellsList[index+i][0] > 0.125 && spellsList[index+i][0] < 0.25) points += 0;
       else if ((spellsList[index+i][0] > 0.375 && spellsList[index+i][0] < 0.5) || (spellsList[index+i][0] > 0.75 && spellsList[index+i][0] < 0.875) || (spellsList[index+i][0] > 0.25 && spellsList[index+i][0] < 2/7)) points += 1;
       else points += 2;
@@ -81,15 +71,10 @@ function isResolvable(index,spellsList,spells) {
     }
   }
   else {
-    for (let i = 1; i < 3; i++) {
+    for (let i = 1; i <= 2; i++) {
       if (index+i >= spellsList.length) return 0;
-
-      if (spellsList[index+i][1] == 0) {
-        spellsList[index+i][1] = getFthofResult(spells+index+i);
-        spellsList[index+i][2] = getFthofResult(spells+index+i,1);
-      }
-      
-      if ((spellsList[index+i][1] === "Building Special" || spellsList[index+i][2] === "Building Special") && spellsList[index+i][0] < 0.5) return 1;
+      if ((spellsList[index+i][1] == 1 || spellsList[index+i][2] == 1)) return 1;
+      else if (i == 2) break;
       else if (spellsList[index+i][0] > 1/7 && spellsList[index+i][0] < 2/7) points += 0;
       else points += 1;
       if (points > 1) return 0;
@@ -100,7 +85,7 @@ function isResolvable(index,spellsList,spells) {
 
 // will not load the mod until grimoire has been loaded
 function checkGrimoire() {
-  var loadedFD = false;
+  let loadedFD = false;
   if (!Game.Objects["Wizard tower"].minigameLoaded) {
     Game.Notify("Grimoire not loaded","Finnless cannot be destroyed if Grimoire is not loaded.",[0,7]);
     Game.registerHook('logic',function(){if (Game.Objects["Wizard tower"].minigameLoaded && !loadedFD) {initFD(); loadedFD = true;}})
@@ -110,26 +95,50 @@ function checkGrimoire() {
   }
 }
 
+function WriteSaveFD(spellLoc) {
+  str = Game.WriteSave(2);
+  spells = String(Game.Objects["Wizard tower"].minigame.spellsCastTotal);
+  pre = str.substring(0,str.indexOf(" "+spells+" "));
+  post = str.substring(str.indexOf(" "+spells+" ")+spells.length+2);
+
+  return Base64.encode(pre+" "+spellLoc+" "+post);
+}
+
+function ExportSaveFD()
+{
+	Game.Prompt('<id ExportSave><h3>'+loc("Export save")+'</h3><div class="block">'+loc("This is your modified save code.<br>Copy it and put it in FtHoF Planner!")+'</div><div class="block"><textarea id="textareaPrompt" style="width:100%;height:128px;" readonly>'+WriteSaveFD(firstComboLoc)+'</textarea></div>',[loc("All done!")]);
+	l('textareaPrompt').focus();l('textareaPrompt').select();
+}
+
 function initFD() {
   Game.Notify("Finnless destroyer","Open options menu to configure mod settings. If a desired seed is found, you will be notified upon reincarnation.",[17,2]);
   resetPrefs();
 
   // Thanks Mr. Lander
-  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`Autosave OFF')+'</div>'+`,`Autosave OFF')+'</div>'+
+  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`created by mods")+')</label></div>':'')+`,`created by mods")+')</label></div>':'')+
     '<div class="listing"><a class="option smallFancyButton"'+Game.clickStr+'="checkSpells();">'+loc("Check with current settings")+'</a><label>('+loc("Run the Finnless Destroyer again with the current settings. Shortcut: shift+F")+')</label></div>'+`));
-  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`game will reload")+')</label><br>'+`,`game will reload")+')</label><br>'+
-    Game.WritePrefButton('notifyFailure','notifyFailureButton',loc("Notify on failure ")+ON,loc("Notify on failure ")+OFF)+'<label>('+loc("when reincarnating, notify even if no results were found")+')</label><br>'+`));
-  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`game will reload")+')</label><br>'+`,`game will reload")+')</label><br>'+
-    Game.WritePrefButton('useSkips','useSkipsButton',loc("Skip SE success & RA ")+ON,loc("Skip SE success & RA ")+OFF)+'<label>('+loc("skip GFD casts of Spontaneous Edifice success and Resurrect Abomination when counting combo length")+')</label><br>'+`));
-  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`game will reload")+')</label><br>'+`,`game will reload")+')</label><br>'+
-    Game.WritePrefButton('checkResolve','checkResolveButton',loc("Check G!FtHoF resolve ")+ON,loc("Check G!FtHoF resolve ")+OFF)+'<label>('+loc("look for nearby building specials that GFD FtHoFs could resolve on, including offset abuse")+')</label><br>'+`));
-  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`Autosave OFF')+'</div>'+`,`Autosave OFF')+'</div>'+
-    '<div class="listing"><a class="option smallFancyButton"'+Game.clickStr+'="setFthofNeeded();">'+loc("Set desired G!FtHoFs")+'</a><label>('+loc("set how many GFD FtHoFs near each other to search for; current value: <b>" + Game.prefs.fthofNeeded + "</b>")+')</label></div>'+`));
-  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`Autosave OFF')+'</div>'+`,`Autosave OFF')+'</div>'+
+  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`created by mods")+')</label></div>':'')+`,`created by mods")+')</label></div>':'')+
+    '<div class="listing">'+Game.WritePrefButton('notifyFailure','notifyFailureButton',loc("Notify on failure ")+ON,loc("Notify on failure ")+OFF)+'<label>('+loc("when reincarnating, notify even if no results were found")+')</label><br>'+'</div>'+`));
+  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`created by mods")+')</label></div>':'')+`,`created by mods")+')</label></div>':'')+
+    '<div class="listing">'+Game.WritePrefButton('useSkips','useSkipsButton',loc("Skip SE success & RA ")+ON,loc("Skip SE success & RA ")+OFF)+'<label>('+loc("skip GFD casts of Spontaneous Edifice success and Resurrect Abomination when counting combo length")+')</label><br>'+'</div>'+`));
+  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`created by mods")+')</label></div>':'')+`,`created by mods")+')</label></div>':'')+
+    '<div class="listing">'+Game.WritePrefButton('checkResolve','checkResolveButton',loc("Check G!FtHoF resolve ")+ON,loc("Check G!FtHoF resolve ")+OFF)+'<label>('+loc("look for nearby building specials that GFD FtHoFs could resolve on, including offset abuse")+')</label><br>'+'</div>'+`));
+  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`created by mods")+')</label></div>':'')+`,`created by mods")+')</label></div>':'')+
     '<div class="listing"><a class="option smallFancyButton"'+Game.clickStr+'="setFthofRange();">'+loc("Set range for G!FtHoFs")+'</a><label>('+loc("set the range within which the GFD FtHoFs must lie; current value: <b>" + Game.prefs.fthofRange + "</b>")+')</label></div>'+`));
-  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`Autosave OFF')+'</div>'+`,`Autosave OFF')+'</div>'+
+  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`created by mods")+')</label></div>':'')+`,`created by mods")+')</label></div>':'')+
+    '<div class="listing"><a class="option smallFancyButton"'+Game.clickStr+'="setFthofNeeded();">'+loc("Set desired G!FtHoFs")+'</a><label>('+loc("set how many GFD FtHoFs near each other to search for; current value: <b>" + Game.prefs.fthofNeeded + "</b>")+')</label></div>'+`));
+  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`created by mods")+')</label></div>':'')+`,`created by mods")+')</label></div>':'')+
     '<div class="listing"><a class="option smallFancyButton"'+Game.clickStr+'="setLookahead();">'+loc("Set lookahead")+'</a><label>('+loc("set the maximum amount of casts to search; current value: <b>" + Game.prefs.lookahead + "</b>")+')</label></div>'+`));
 
+  // FD heading+desc
+  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`created by mods")+')</label></div>':'')+`,`created by mods")+')</label></div>':'')+
+    '<div class="listing">'+loc("Finnless Destroyer is a mod designed for the Finnless ruleset, similar to the combo finder but implemented into the game. It's designed for very large finnless combos (decacasts or higher).<br><br>For more information and a tutorial about this mod, see <a href='//tinyurl.com/FinnlessDestroyer' target='_blank'>this guide</a>.")+'</div>'+`));
+  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`created by mods")+')</label></div>':'')+`,`created by mods")+')</label></div>':'')+
+    '<div class="title">'+loc("Finnless Destroyer")+'</div>'+`));
+
+  eval('Game.UpdateMenu='+Game.UpdateMenu.toString().replace(`import: ctrl+O)")+'</label></div>'+`,`import: ctrl+O)")+'</label></div>'+
+    '<div class="listing"><a class="option smallFancyButton" '+Game.clickStr+'="ExportSaveFD();PlaySound(\`snd/tick.mp3\`);">'+loc("Export FD save")+'</a><label>'+loc("Creates a modified save file where the number of spells cast matches the first result found by Finnless Destroyer")+'</label></div>'+`));
+  
   // shift+F to check spells again
   AddEvent(window,'keydown',function(e) {
     if (e.shiftKey && e.keyCode==70) checkSpells();
@@ -158,18 +167,50 @@ function checkSpells() {
   let spells = Game.Objects["Wizard tower"].minigame.spellsCastTotal;
   let maxLength = Game.prefs.fthofRange;
 
-  // get all results in an aray before analyzing
+  // get all results in an array before analyzing
   let spellsList = [ ];
+  let checkFthof = 0;
   for (let i = spells; i < spells + Game.prefs.lookahead; i++) {
     let spellRes = [0,0,0];
     Math.seedrandom(Game.seed+'/'+i);
     spellRes[0] = Math.random();
+
+    if (checkFthof > 0 && spellRes[0] < 0.5) {
+      Math.random();
+      Math.random();
+      let call0 = Math.random();
+      let call1 = Math.random();
+      let call2 = Math.random();
+      let call3 = Math.random();
+      let call4 = Math.random();
+      let call5 = Math.random();
+
+      if (call1 < 0.25 && call2 > 0.15) {
+        let numElements = 3;
+        if (call0 < 0.1) numElements+=3;
+        let bsIndex = numElements;
+        numElements++;
+        if (call3 < 0.0001) numElements++;
+        if (call4 > bsIndex/numElements && call4 < (bsIndex+1)/numElements) spellRes[1] = 1;
+      }
+      if (spellRes[1] == 0 && call2 < 0.25 && call3 > 0.15) {
+        let numElements = 3;
+        if (call1 < 0.1) numElements+=3;
+        let bsIndex = numElements;
+        numElements++;
+        if (call4 < 0.0001) numElements++;
+        if (call5 > bsIndex/numElements && call5 < (bsIndex+1)/numElements) spellRes[2] = 1;
+      }
+
+      if (spellRes[1]+spellRes[2] > 0) checkFthof = 0;
+    }
+    if (checkFthof > 0) checkFthof--;
+    
+    if (spellRes[0] > 0.125 && spellRes[0] < 0.25) checkFthof = 7;
+    else if (spellRes[0] > 0.25 && spellRes[0] < 2/7) checkFthof = Math.max(checkFthof,2);
     spellsList.push(spellRes);
   }
-
-  // don't wanna pull an orteil
-  Math.seedrandom();
-    
+  
   for (let i = 0; i < Game.prefs.lookahead; i++)
   {
     // add g!fthofs to array
@@ -183,9 +224,8 @@ function checkSpells() {
         maxLength++;
       }
       // se success
-      else if (spellsList[i][0] > 0.375 && spellsList[i][0] < 0.5) {
-        Math.seedrandom(Game.seed+'/'+(i+1));
-        if (Math.random() < 0.5) {
+      else if (spellsList[i][0] > 0.375 && spellsList[i][0] < 0.5 && i+1 < Game.prefs.lookahead) {
+        if (spellsList[i+1][0] < 0.5) {
           skips.push(i)
           maxLength++;
         }
@@ -204,6 +244,9 @@ function checkSpells() {
     }
     else if (fthofs.length >= Game.prefs.fthofNeeded) neededFthofsLocs.push([i+1+spells,maxLength]);
   }
+
+  // don't wanna pull an orteil
+  Math.seedrandom();
     
   if (neededFthofsLocs.length > 0) {
     PlaySound('snd/spell.mp3');
@@ -212,10 +255,12 @@ function checkSpells() {
     for (let i = 0; i < neededFthofsLocs.length; i++) {
       neededFthofsLocs[i] = neededFthofsLocs[i][0]-neededFthofsLocs[i][1];
     }
+    firstComboLoc = neededFthofsLocs[0];
     console.log(neededFthofsLocs);
     Game.Notify("Success!","Total of " + neededFthofsLocs.length + " locations found.<br><br>The first is at spell no. <b>" + neededFthofsLocs[0] + "</b>.<br><br> Go to the console to see them all!",[17,2]);
   }
   else if (Game.prefs.notifyFailure == 1) {
+    firstComboLoc = spells;
     PlaySound('snd/spellFail.mp3');
     Game.Notify("Failure...","No locations with specified settings were found.",[17,15]);
   }
